53daedc443873ee6f6b47de67f231e756706d85e
Yanick Senn <yanick.senn@bluewin.ch>
2019-09-23 17:24:04 +0200

    Added bracket-test for letters

--- ExBox/src/ch/zhaw/rhiana/ads/Praktikum01/BracketServerTest.java
+++ ExBox/src/ch/zhaw/rhiana/ads/Praktikum01/BracketServerTest.java
@@ -3,27 +3,38 @@ package ch.zhaw.rhiana.ads.Praktikum01;
 import org.junit.Test;
 import org.junit.Before;
 import static org.junit.Assert.*;
+
+
 public class BracketServerTest {
-	
-BracketServer bs;
-	
+
+	BracketServer bs;
+
 	@Before
 	public void setUp() throws Exception {
 		bs = new BracketServer();
 	}
+
 	private void test(String s, boolean b) {
-		assertEquals(s,bs.checkBrackets(s),b);
+		assertEquals(s, bs.checkBrackets(s), b);
+	}
+
+	@Test
+	public void testBracket() {
+		test("()", true);
+		test("(()]", false);
+		test("((([([])])))", true);
+		test("[(])", false);
 	}
 	
 	@Test
-	public void testBracket() {
-		test("()",true);
-		test("(()]",false);
-		test("((([([])])))",true);
-		test("[(])",false);
-		test("[(3 +3)* 35 +3]* {3 +2}",true);
-		test("[({3 +3)* 35} +3]* {3 +2}",false);
+	public void testBracketsWithNumbers() {
+		test("[(3 +3)* 35 +3]* {3 +2}", true);
+		test("[({3 +3)* 35} +3]* {3 +2}", false);
 	}
-
 	
+	@Test
+	public void testBracketsWithLetters() {
+		test("abc(def{ghi[jkl]mno}pqr)stu", true);
+		test("abc[def{ghi[jkl)mno}pqr}stu", false);
+	}
 }
