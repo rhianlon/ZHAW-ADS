888db16745e2b20b4031a6b002647534ae17e148
Yanick Senn <yanicksenn@yanicks-mbp.zhaw.ch>
2019-10-09 17:10:45 +0200

    added command to SnowFlakeServer

--- ExBox/src/ch/zhaw/rhiana/ads/Praktikum04/SnowFlakeServer.java
+++ ExBox/src/ch/zhaw/rhiana/ads/Praktikum04/SnowFlakeServer.java
@@ -1,12 +1,21 @@
 package ch.zhaw.rhiana.ads.Praktikum04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ch.zhaw.rhiana.ads.CommandExecutor;
 
 public class SnowFlakeServer implements CommandExecutor {
 
 	@Override
 	public String execute(String command) throws Exception {
-		
+		String[] myCommandString = command.split(" ");
+		if(myCommandString.length != 2) {
+			throw new IllegalArgumentException("Illegal amount of arguments was presented, please give level and distance in int");
+		}
+		int level = Integer.valueOf(myCommandString[0]);
+		double distance = Double.valueOf(myCommandString[1]);
+		renderSnowflake(level, distance);
 		
 		return null;
 	}
--- ExBox/src/ch/zhaw/rhiana/ads/Praktikum04/TestGraphicServer.java
+++ ExBox/src/ch/zhaw/rhiana/ads/Praktikum04/TestGraphicServer.java
@@ -5,7 +5,8 @@ import ch.zhaw.rhiana.ads.CommandExecutor;
 public class TestGraphicServer implements CommandExecutor {
 
 
-    String figure = "<line x1=\"0.1\" y1 = \"0.8\" x2=\"0.9\" y2 = \"0.8\" />\n"+
+    String figure = 
+    		"<line x1=\"0.1\" y1 = \"0.8\" x2=\"0.9\" y2 = \"0.8\" />\n"+
             "<line x1=\"0.2\" y1 = \"0.4\" x2=\"0.2\" y2 = \"0.8\" />\n"+
             "<line x1=\"0.3\" y1 = \"0.4\" x2=\"0.3\" y2 = \"0.8\" />\n"+
             "<line x1=\"0.3\" y1 = \"0.4\" x2=\"0.7\" y2 = \"0.4\" />\n"+
@@ -18,5 +19,4 @@ public class TestGraphicServer implements CommandExecutor {
     public String execute(String command) {
         return figure;
     }
-
 }
